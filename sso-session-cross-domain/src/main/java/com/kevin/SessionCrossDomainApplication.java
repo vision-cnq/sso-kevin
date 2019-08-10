841bf1aa4cc8089cfd2084f10899eb082777aef4
caonanqing <caonanqing@foxmail.com>
2019-08-10 16:00:59 +0800

    新增项目，新增module，session跨域

---
+++ sso-session-cross-domain/src/main/java/com/kevin/SessionCrossDomainApplication.java
@@ -0,0 +1,18 @@
+package com.kevin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author caonanqing
+ * @version 1.0
+ * @description     session跨域
+ * @createDate 2019/8/10
+ */
+@SpringBootApplication
+public class SessionCrossDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionCrossDomainApplication.class,args);
+    }
+}
